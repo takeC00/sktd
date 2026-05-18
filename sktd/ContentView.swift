08e5b52d2b6a81bf1d43701598ec61dd37ea4710
takeC00 <take.614@icloud.com>
2026-05-19 08:07:40 +0900

    first

---
+++ sktd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sktd
+//
+//  Created by 服部剛士 on 2026/05/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sktd/sktdApp.swift
@@ -0,0 +1,17 @@
+//
+//  sktdApp.swift
+//  sktd
+//
+//  Created by 服部剛士 on 2026/05/18.
+//
+
+import SwiftUI
+
+@main
+struct sktdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
